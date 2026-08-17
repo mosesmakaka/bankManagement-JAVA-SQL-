4785ad4db1386ef2a55e25ff0e0258b32822be49
Moses Makaka <53109687+mosesmakaka@users.noreply.github.com>
2026-08-17 09:39:41 +0000

    Test 1

---
+++ src/bank/management/system/practice.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.awt.*;
+
+class MyDrawPanel extends JPanel    {
+    public void paintComponent(Graphics g)  {
+        g.setColor(Color.orange);
+
+        g.fillRect(20, 50, 100, 100);
+    }
+}
+public static void main() {
+    // Displaying the rectangle
+    
+}
